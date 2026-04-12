7262d59ac02feb0c0195dab23dc729c38d2d4a86
Aws1811 <awssleebi181@gmail.com>
2026-04-12 20:01:03 +0300

    before Grid

---
+++ web fundementals/css adv/Z-index/script.js
@@ -0,0 +1,6 @@
+var theDiv = document.querySelector("section");
+var theBlack = document.querySelector(".black");
+function hide(elem) {
+    theDiv.style.display  = "none";
+    theBlack.style.display  = "none";
+}
