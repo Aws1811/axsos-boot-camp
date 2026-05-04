7395c59e6aa5ab3779f25653f36398717f01f4f7
Aws1811 <awssleebi181@gmail.com>
2026-05-04 13:56:35 +0300

    python

---
+++ python_stack/_python/OOP/python.py
@@ -0,0 +1,19 @@
+# 1 hello world
+print("hello world")
+
+# 2 hello noelle
+name = 'noelle'
+print('hello' , name)
+print('hello' + name)
+
+# 3 hello 42
+num = 42
+print('hello' + f" {num}")
+print('hello' , num)
+
+# 4 food print
+fav_f = "sushi"
+fav_ff = "pizza"
+print("I love to eat {} and {}".format(fav_f,fav_ff) )
+print(f"I love to eat {fav_f} and {fav_ff}")
+
